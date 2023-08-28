9072cdabb07ce04d40e7f67e9017ded8cd85816f
JarousNemec <jarous.nemec@gmail.com>
2023-08-28 16:15:17 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,411 @@
+#include "main.h"
+
+void setup() {
+    Serial.begin(9600);
+
+    //Init Transceiver
+    ESerial.begin(9600);
+    Serial.println("Starting Chat");
+    Serial.println(Transceiver.init());
+    Transceiver.PrintParameters();
+
+    //Init web server and Wi-Fi ap
+    randomSeed(analogRead(A0));
+//    WiFi.softAP(ssid + String(random(300)), password);
+    WiFi.softAP(ssid);
+    WiFi.softAPConfig(local_ip, gateway, subnet);
+    delay(100);
+    server.on("/", HandleOnConnect);
+    server.onNotFound(HandleNotFound);
+    server.begin();
+    Serial.println("HTTP server started");
+
+    pinMode(PIN_SUCCESS_LED, OUTPUT);
+    pinMode(PIN_SEND_LED, OUTPUT);
+
+    //Load configuration
+    LoadConfiguration();
+}
+
+void loop() {
+    server.handleClient();
+    if (ESerial.available()) {
+        String input = ESerial.readString();
+        JsonObject &IncommingObj = jBuffer.parseObject(input);
+        if (ValidatePacket(&IncommingObj)) {
+            ProcessPacket(&IncommingObj);
+        }
+    }
+    BehaveByStationType();
+    ShineControl();
+    jBuffer.clear();
+}
+
+void ShineControl() {
+    if ((millis() - shine_send_time) > LED_TIMEOUT) {
+        digitalWrite(PIN_SEND_LED, LOW);
+        shine_send_time = 0;
+    }
+
+    if ((millis() - shine_success_time) > LED_TIMEOUT) {
+        digitalWrite(PIN_SUCCESS_LED, LOW);
+        shine_success_time = 0;
+    }
+}
+
+void ShineSend() {
+    shine_send_time = millis();
+    digitalWrite(PIN_SEND_LED, HIGH);
+}
+
+void ShineSuccess() {
+    shine_success_time = millis();
+    digitalWrite(PIN_SUCCESS_LED, HIGH);
+}
+
+void BehaveByStationType() {
+    ValidateTimeouts();
+    switch (StationConfig.SType) {
+        case Beacon:
+            break;
+        case FieldStation:
+            BehaveAsFieldStation();
+            break;
+        case Bridge:
+            break;
+        case Pinger:
+            BehaveAsPinger();
+            break;
+        case UndefinedS:
+            break;
+    }
+}
+
+void ValidateTimeouts() {
+    if (millis() - PingStatus.PingSentWhen > PING_TIMEOUT)
+        PingStatus.PingSent = false;
+    if (millis() - BeaconDiscoverStatus.DiscoverSentWhen > DISCOVER_TIMEOUT)
+        BeaconDiscoverStatus.DiscoverSent = false;
+    if (millis() - OtherDataStatus.OtherDataSentWhen > OTHER_DATA_TIMEOUT)
+        OtherDataStatus.OtherDataSent = false;
+}
+
+void BehaveAsPinger() {
+    if (BeaconId == "") {
+        if (!BeaconDiscoverStatus.DiscoverSent) {
+            Serial.println("Discover sent!");
+            SendPacket(PacketType::Syn, "DiscB", StationConfig.Id, BeaconDiscoverStatus.DiscoverTarget);
+            BeaconDiscoverStatus.DiscoverSent = true;
+            BeaconDiscoverStatus.DiscoverSentWhen = millis();
+        }
+    } else {
+        if (!PingStatus.PingSent) {
+            Serial.println("Ping sent!");
+            SendPacket(PacketType::Syn, "Ping", StationConfig.Id, BeaconId);
+            PingStatus.PingSent = true;
+            PingStatus.PingSentWhen = millis();
+            PingStatus.PingSentTo = BeaconId;
+        }
+    }
+}
+
+void BehaveAsFieldStation() {
+    if (BeaconId == "") {
+        if (!BeaconDiscoverStatus.DiscoverSent) {
+            Serial.println("Discover sent!");
+            SendPacket(PacketType::Syn, "DiscB", StationConfig.Id, BeaconDiscoverStatus.DiscoverTarget);
+            BeaconDiscoverStatus.DiscoverSent = true;
+            BeaconDiscoverStatus.DiscoverSentWhen = millis();
+        }
+    } else {
+        if (!OtherDataStatus.OtherDataSent && OtherDataStatus.OtherDataCollected &&
+            OtherDataStatus.OtherDataRequested) {
+            Serial.println("Other data sent!");
+            SendPacket(PacketType::Syn, "OD", StationConfig.Id, BeaconId);
+//            fadf;
+            OtherDataStatus.OtherDataSent = true;
+            OtherDataStatus.OtherDataSentWhen = millis();
+            OtherDataStatus.OtherDataTarget = BeaconId;
+        }
+    }
+}
+
+void PrintPacketStructure(JsonObject *pckt) {
+    Serial.print("Packet sent:");
+    Serial.print((pckt->get<String>("t")));
+    Serial.print(": ");
+    Serial.print((pckt->get<String>("c")));
+    Serial.print(": ");
+    Serial.print((pckt->get<String>("s")));
+    Serial.print(": ");
+    Serial.println((pckt->get<String>("d")));
+}
+
+void SendPacket(PacketType type, const String &content, const String &source, const String &destination) {
+    JsonObject &PacketToSend = jBuffer.createObject();
+    PacketToSend["t"] = (int) type;
+    PacketToSend["c"] = content;
+    PacketToSend["s"] = source;
+    PacketToSend["d"] = destination;
+    String output;
+    PacketToSend.printTo(output);
+    ESerial.print(output);
+    PrintPacketStructure(&PacketToSend);
+    ShineSend();
+}
+
+void ProcessPacket(JsonObject *pckt) {
+    String content = pckt->get<String>("c");
+    if (content == "DiscB") {
+        ProcessDiscoverBeacon(pckt);
+    } else if (content == "Ping") {
+        ProcessPing(pckt);
+    } else if (content.substring(0, 2) == "OD") {
+        ProcessOtherData(pckt);
+    }
+}
+
+void CollectSensorData() {
+
+}
+
+void ProcessOtherData(JsonObject *pckt) {
+    auto source = pckt->get<String>("s");
+    auto destination = pckt->get<String>("d");
+    auto type = (PacketType) pckt->get<int>("t");
+    switch (type) {
+        case Syn:
+            if (destination == StationConfig.Id) {
+                OtherDataStatus.OtherDataRequested = true;
+                CollectSensorData();
+            }
+            break;
+        case SynAck:
+            if (OtherDataStatus.OtherDataSent && OtherDataStatus.OtherDataTarget == source) {
+                Serial.print("Successfully got data from:");
+                Serial.println(source);
+                OtherDataStatus.OtherDataSent = false;
+                OtherDataStatus.OtherDataSentWhen = 0;
+                OtherDataStatus.OtherDataTarget = "";
+//                afsdfsd;
+                SendPacket(PacketType::Ack, "OD", StationConfig.Id, source);
+                ShineSuccess();
+            }
+            break;
+        case Ack:
+            if (OtherDataStatus.OtherDataSent && OtherDataStatus.OtherDataTarget == source) {
+                Serial.print("Successfully delivered data to:");
+                Serial.println(source);
+                OtherDataStatus.OtherDataSent = false;
+                OtherDataStatus.OtherDataSentWhen = 0;
+                OtherDataStatus.OtherDataTarget = "";
+                OtherDataStatus.OtherDataCollected = false;
+                OtherDataStatus.OtherDataRequested = false;
+                ShineSuccess();
+            }
+            break;
+    }
+}
+
+void ProcessPing(JsonObject *pckt) {
+    auto source = pckt->get<String>("s");
+    auto destination = pckt->get<String>("d");
+    auto type = (PacketType) pckt->get<int>("t");
+    switch (type) {
+        case Syn:
+            if (destination == StationConfig.Id) {
+                SendPacket(PacketType::SynAck, "Ping", StationConfig.Id, source);
+                PingStatus.PingSent = true;
+                PingStatus.PingSentTo = source;
+                PingStatus.PingSentWhen = millis();
+            }
+            break;
+        case SynAck:
+            if (PingStatus.PingSent && PingStatus.PingSentTo == source) {
+                Serial.print("Successfully ping id:");
+                Serial.println(source);
+                PingStatus.PingSent = false;
+                PingStatus.PingSentWhen = 0;
+                PingStatus.PingSentTo = "all";
+                SendPacket(PacketType::Ack, "Ping", StationConfig.Id, source);
+                ShineSuccess();
+            }
+            break;
+        case Ack:
+            if (PingStatus.PingSent && PingStatus.PingSentTo == source) {
+                Serial.print("Successfully ping id:");
+                Serial.println(source);
+                PingStatus.PingSent = false;
+                PingStatus.PingSentWhen = 0;
+                PingStatus.PingSentTo = "all";
+                ShineSuccess();
+            }
+            break;
+    }
+}
+
+void ProcessDiscoverBeacon(JsonObject *pckt) {
+    auto source = pckt->get<String>("s");
+    auto destination = pckt->get<String>("d");
+    auto type = (PacketType) pckt->get<int>("t");
+    switch (type) {
+        case Syn:
+            if (StationConfig.SType == Beacon) {
+                SendPacket(PacketType::SynAck, "DiscB", StationConfig.Id, source);
+                BeaconDiscoverStatus.DiscoverSent = true;
+                BeaconDiscoverStatus.DiscoverTarget = source;
+                BeaconDiscoverStatus.DiscoverSentWhen = millis();
+            }
+            break;
+        case SynAck:
+            if (BeaconDiscoverStatus.DiscoverSent && source != "" &&
+                (StationConfig.SType == Pinger || StationConfig.SType == FieldStation)) {
+                Serial.print("Successfully discovered beacon id:");
+                Serial.println(source);
+                BeaconId = source;
+                BeaconDiscoverStatus.DiscoverSent = false;
+                BeaconDiscoverStatus.DiscoverSentWhen = 0;
+                BeaconDiscoverStatus.DiscoverTarget = "all";
+                SendPacket(PacketType::Ack, "DiscB", StationConfig.Id, source);
+                ShineSuccess();
+            }
+            break;
+        case Ack:
+            if (StationConfig.SType == Beacon && source == BeaconDiscoverStatus.DiscoverTarget &&
+                BeaconDiscoverStatus.DiscoverSent) {
+                Serial.println("Beacon successfully discovered new station!");
+                ShineSuccess();
+            }
+            break;
+    }
+}
+
+bool ValidatePacket(JsonObject *pckt) {
+    return pckt->containsKey("t") && pckt->containsKey("s") && pckt->containsKey("d") && pckt->containsKey("c");
+}
+
+String SendHTML() {
+    String ptr = "<!DOCTYPE html>\n"
+                 "<html lang=\"en\">\n"
+                 "<head>\n"
+                 "    <meta charset=\"UTF-8\">\n"
+                 "    <title>Station control</title>\n"
+                 "</head>\n"
+                 "<body>\n"
+                 "    <h3>Actual set Id: " + String(StationConfig.Id) + "</h3><h3>Actual set Type: " + String(GetStationTypeFromEnum()) + "</h3><br>\n"
+                 "    <form action=\"/\" method=\"post\">\n"
+                 "        <label for=\"ssid\">WWW ap SSID (only for beacon):</label>\n"
+                 "        <input type=\"text\" id=\"ssid\" name=\"ssid\" value=\"" + String(StationConfig.SSID) + "\"><br>\n"
+                 "        <label for=\"psw\">WWW ap PSW (only for beacon):</label>\n"
+                 "        <input type=\"text\" id=\"psw\" name=\"psw\" value=\"" + String(StationConfig.PSW) + "\"><br>\n"
+                 "        <label for=\"id\">ID:</label>\n"
+                 "        <input type=\"text\" id=\"id\" name=\"id\" value=\"" + String(StationConfig.Id) + "\"><br>\n"
+                 "        <label>Station Type:</label>\n"
+                 "        <input type=\"radio\" id=\"beacon\" name=\"type\" value=\"Beacon\" " + (StationConfig.SType == Beacon ? "checked" : "") + " ><label for=\"beacon\" >Beacon</label>\n"
+                 "        <input type=\"radio\" id=\"fieldStation\" name=\"type\" value=\"FieldStation\" " + (StationConfig.SType == FieldStation ? "checked" : "") + " ><label for=\"fieldStation\">FieldStation</label>\n"
+                 "        <input type=\"radio\" id=\"bridge\" name=\"type\" value=\"Bridge\"><label for=\"bridge\" " + (StationConfig.SType == Bridge ? "checked" : "") + " >Bridge</label>\n"
+                 "        <input type=\"radio\" id=\"pinger\" name=\"type\" value=\"Pinger\"><label for=\"pinger\" " + (StationConfig.SType == Pinger ? "checked" : "") + " >Pinger</label><br>\n"
+                 "        <label>FieldStation Type:</label>\n"
+                 "        <input type=\"radio\" id=\"beacon\" name=\"type\" value=\"Beacon\" " + (StationConfig.SType == Beacon ? "checked" : "") + " ><label for=\"beacon\" >Beacon</label>\n"
+                 "        <input type=\"radio\" id=\"fieldStation\" name=\"type\" value=\"FieldStation\" " + (StationConfig.SType == FieldStation ? "checked" : "") + " ><label for=\"fieldStation\">FieldStation</label>\n"
+                 "        <input type=\"radio\" id=\"bridge\" name=\"type\" value=\"Bridge\"><label for=\"bridge\" " + (StationConfig.SType == Bridge ? "checked" : "") + " >Bridge</label>\n"
+                 "        <input type=\"radio\" id=\"pinger\" name=\"type\" value=\"Pinger\"><label for=\"pinger\" " + (StationConfig.SType == Pinger ? "checked" : "") + " >Pinger</label><br><br>\n"
+                 "        <input type=\"submit\" value=\"Submit\">"
+                 "    </form>\n"
+                 "</body>\n"
+                 "</html>";
+    return ptr;
+}
+
+void HandleOnConnect() {
+
+    if (server.method() == HTTP_POST && server.args() == 2) {
+        if (server.argName(0) == "id" && server.argName(1) == "type") {
+            server.arg(0).toCharArray(StationConfig.Id, 128);
+            StationConfig.SType = GetStationTypeFromString(server.arg(1));
+            SaveConfiguration();
+            Serial.println("Configuration writed.");
+            LoadConfiguration();
+        }
+    }
+    server.send(200, "text/html", SendHTML());
+}
+
+void HandleNotFound() {
+    server.send(404, "text/plain", "Not found");
+}
+
+
+StationType GetStationTypeFromString(const String &text) {
+    if (text == "FieldStation") {
+        return FieldStation;
+    } else if (text == "Beacon") {
+        return Beacon;
+    } else if (text == "Bridge") {
+        return Bridge;
+    } else if (text == "Pinger") {
+        return Pinger;
+    }
+    return UndefinedS;
+}
+
+String GetStationTypeFromEnum() {
+    switch (StationConfig.SType) {
+        case Beacon:
+            return "Beacon";
+        case FieldStation:
+            return "FieldStation";
+        case Bridge:
+            return "Bridge";
+        case Pinger:
+            return "Pinger";
+        default:
+            return "UndefinedS";
+    }
+}
+
+FieldStationType GetFieldStationTypeFromString(const String &text) {
+    if (text == "WaterSensor") {
+        return WaterSensor;
+    } else if (text == "SolarPanelController") {
+        return SolarPanelController;
+    }
+    return UndefinedFS;
+}
+
+String GetFieldStationTypeFromEnum(){
+    switch (StationConfig.FSType) {
+        case WaterSensor:
+            return "WaterSensor";
+        case SolarPanelController:
+            return "SolarPanelController";
+        default:
+            return "UndefinedFS";
+    }
+
+
+}
+
+void SaveConfiguration() {
+    EEPROM.begin(512);
+    EEPROM.put(CONFIGURATION_ADDRESS, StationConfig);
+    boolean ok = EEPROM.commit();
+    Serial.println((ok) ? "Commit OK" : "Commit failed");
+    EEPROM.end();
+}
+
+void LoadConfiguration() {
+    EEPROM.begin(512);
+    EEPROM.get(CONFIGURATION_ADDRESS, StationConfig);
+    EEPROM.end();
+    PrintConfig();
+}
+
+void PrintConfig() {
+    Serial.println("Loaded configuration...");
+    Serial.print("Id: ");
+    Serial.println(StationConfig.Id);
+    Serial.print("Type:");
+    Serial.println(GetStationTypeFromEnum());
+    Serial.println("----------------");
+}
---
+++ src/main.h
@@ -0,0 +1,143 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include "../lib/EBYTE/EBYTE.h"
+#include "../lib/ArduinoJson/ArduinoJson.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "../lib/ESP_EEPROM/src/ESP_EEPROM.h"
+
+#ifndef LORARANGEMETER_MAIN_H
+#define LORARANGEMETER_MAIN_H
+
+#define PIN_RX 14   //D5 on the board (Connect this to the EBYTE TX pin)
+#define PIN_TX 12   //D6 on the board (connect this to the EBYTE RX pin)
+
+#define PIN_M0 5    //D1 on the board (connect this to the EBYTE M0 pin)
+#define PIN_M1 4    //D2 on the board (connect this to the EBYTE M1 pin)
+#define PIN_AX 16   //D0 on the board (connect this to the EBYTE AUX pin)
+#define PIN_SUCCESS_LED 13
+#define PIN_SEND_LED 15
+#define CONFIGURATION_ADDRESS 1
+#define DISCOVER_TIMEOUT 12000
+#define PING_TIMEOUT 4000
+#define OTHER_DATA_TIMEOUT 5000
+#define LED_TIMEOUT 500
+// you will need to define the pins to create the serial port
+SoftwareSerial ESerial(PIN_RX, PIN_TX);
+
+// create the transceiver object, passing in the serial and pins
+EBYTE Transceiver(&ESerial, PIN_M0, PIN_M1, PIN_AX);
+
+DynamicJsonBuffer jBuffer;
+
+enum PacketType {
+    Syn = 1, SynAck = 2, Ack = 3
+};
+
+enum StationType {
+    Beacon, FieldStation, Bridge, UndefinedS, Pinger
+};
+enum FieldStationType {
+    UndefinedFS,WaterSensor, SolarPanelController
+};
+
+struct ConfigStruct {
+    char Id[128]{};
+    char SSID[128]{};
+    char PSW[128]{};
+    StationType SType = UndefinedS;
+    FieldStationType FSType = UndefinedFS;
+} StationConfig;
+
+struct PingStatusStruct {
+    bool PingSent = false;
+    String PingSentTo = "";
+    unsigned long PingSentWhen = 0;
+} PingStatus;
+
+struct BeaconDiscoverStatusStruct {
+    bool DiscoverSent = false;
+    unsigned long DiscoverSentWhen = 0;
+    String DiscoverTarget = "all";
+} BeaconDiscoverStatus;
+
+struct OtherDataStatusStruct {
+    bool OtherDataSent = false;
+    bool OtherDataRequested = false;
+    bool OtherDataCollected = false;
+    unsigned long OtherDataSentWhen = 0;
+    String OtherDataTarget = "";
+    String OtherData = "";
+} OtherDataStatus;
+
+String BeaconId = "";
+const String ssid = "LoraNet";  // Enter SSID here
+const char *password = "12345678";  //Enter Password here
+
+unsigned long shine_success_time = 0;
+unsigned long shine_send_time = 0;
+
+/* Put IP Address details */
+IPAddress local_ip(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+ESP8266WebServer server(80);
+
+String SendHTML();
+
+void HandleNotFound();
+
+void HandleOnConnect();
+
+StationType GetStationTypeFromString(const String &text);
+
+String GetStationTypeFromEnum();
+
+void LoadConfiguration();
+
+void PrintConfig();
+
+void SaveConfiguration();
+
+bool ValidatePacket(JsonObject *pckt);
+
+void ProcessPacket(JsonObject *pckt);
+
+void ProcessPing(JsonObject *pckt);
+
+void BehaveByStationType();
+
+void BehaveAsPinger();
+
+void SendPacket(PacketType type, const String& content, const String& source, const String& destination);
+
+void ProcessDiscoverBeacon(JsonObject *pckt);
+
+void ValidateTimeouts();
+
+void ShineControl();
+
+void ShineSend();
+
+void ShineSuccess();
+
+void BehaveAsFieldStation();
+
+void ProcessOtherData(JsonObject *pckt);
+
+void CollectSensorData();
+
+FieldStationType GetFieldStationTypeFromString(const String &text);
+
+String GetFieldStationTypeFromEnum();
+
+#endif //LORARANGEMETER_MAIN_H
+
+
+/*<<<<Ping Packet Struct>>>>
+ * t - type
+ * c - content
+ * s - from device name
+ * d - to device name
+ * */
